6739fd6d30fb1f28a847514ad27baaff3467ca9e
Barış Parlan <bparlan@gmail.com>
2018-05-20 20:54:52 +0300

    Working v.0.1

--- regina.py
+++ regina.py
@@ -6,28 +6,191 @@
 import settings # settings.py holds apikey variable.
 import speech_recognition as sr # https://realpython.com/python-speech-recognition/
 import os
+from sys import byteorder
+from array import array
+from struct import pack
+
+import pyaudio
+import wave
+
+import wikipedia
+import pyttsx3
+
+# pip install pypiwin32
+# pip install pyttsx3
+# pip install wikipedia
+# pip install pyaudio
+
+THRESHOLD = 500
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
 
 script_dir = os.path.dirname(__file__)
-wav_location = script_dir + "/harvard.wav"
+wav_location = script_dir + "/demo.wav"
 
-r = sr.Recognizer()
-harvard = sr.AudioFile(wav_location)
-with harvard as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.record(source)
+busy = False
 
-type(audio)
+def is_silent(snd_data):
+    "Returns 'True' if below the 'silent' threshold"
+    return max(snd_data) < THRESHOLD
 
-wav_text = str(r.recognize_google(audio))
-print(wav_text)
+def normalize(snd_data):
+    "Average the volume out"
+    MAXIMUM = 16384
+    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
 
+    r = array('h')
+    for i in snd_data:
+        r.append(int(i*times))
+    return r
+
+def trim(snd_data):
+    "Trim the blank spots at the start and end"
+    def _trim(snd_data):
+        snd_started = False
+        r = array('h')
+
+        for i in snd_data:
+            if not snd_started and abs(i)>THRESHOLD:
+                snd_started = True
+                r.append(i)
+
+            elif snd_started:
+                r.append(i)
+        return r
+
+    # Trim to the left
+    snd_data = _trim(snd_data)
+
+    # Trim to the right
+    snd_data.reverse()
+    snd_data = _trim(snd_data)
+    snd_data.reverse()
+    return snd_data
+
+def add_silence(snd_data, seconds):
+    "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
+    r = array('h', [0 for i in range(int(seconds*RATE))])
+    r.extend(snd_data)
+    r.extend([0 for i in range(int(seconds*RATE))])
+    return r
+
+def record():
+    """
+    Record a word or words from the microphone and
+    return the data as an array of signed shorts.
+
+    Normalizes the audio, trims silence from the
+    start and end, and pads with 0.5 seconds of
+    blank sound to make sure VLC et al can play
+    it without getting chopped off.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=1, rate=RATE,
+        input=True, output=True,
+        frames_per_buffer=CHUNK_SIZE)
+
+    num_silent = 0
+    snd_started = False
+
+    r = array('h')
+
+    while 1:
+        # little endian, signed short
+        snd_data = array('h', stream.read(CHUNK_SIZE))
+        if byteorder == 'big':
+            snd_data.byteswap()
+        r.extend(snd_data)
+
+        silent = is_silent(snd_data)
+
+        if silent and snd_started:
+            num_silent += 1
+        elif not silent and not snd_started:
+            snd_started = True
+
+        if snd_started and num_silent > 30:
+            break
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = normalize(r)
+    #r = trim(r)
+    #r = add_silence(r, 0.5)
+    return sample_width, r
+
+def record_to_file(path):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = record()
+    data = pack('<' + ('h'*len(data)), *data)
+
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+    print("Wav Recorded: " + path)
+
+def speech_to_text():
+    r = sr.Recognizer()
+    harvard = sr.AudioFile(wav_location)
+    with harvard as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.record(source)
+
+    type(audio)
+
+    wav_text = r.recognize_google(audio)
+    os.remove(wav_location)
+    print("Wav's text: " + wav_text)
+    return wav_text
+
+    # question["question"] = question["voice_question"]
+    # print("question.soru saves as text format.")
+
+def search_in_wiki(keyword):
+    wikipedia.set_lang("en")
+    tts_text = str(wikipedia.summary(keyword, sentences=1))
+    return tts_text
+
+def text_to_speech(to_be_read):
+    engine = pyttsx3.init()
+    engine.setProperty('rate',120)  #120 words per minute
+    engine.setProperty('volume',0.9)
+
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.say(to_be_read)
+    engine.runAndWait()
+    engine.stop()
+
+def listen():
+    print("please speak a word into the microphone")
+    record_to_file(wav_location)
+    busy == True
+    query = speech_to_text()
+    text_to_speech("I am searching for " + query)
+    text_to_speech(search_in_wiki(query))
+    busy == False
+
+if __name__ == '__main__':
+    while busy == False:
+        listen()
+
+
+"""
 # https://realpython.com/python-speech-recognition/
 
 setting = { "search_engine" : "duckduckgo" }
 question = {"type" : "research_summary", "word_count" : "3", "results" : "", "voice_question" : ""}
 answer = {}
 
-"""
+
 TODO: Git exclude api keys.
 
 * Speech Recognition
@@ -42,15 +205,32 @@ TODO: Git exclude api keys.
   Gtts to save
   [gTTS](https://github.com/pndurette/gTTS)
 
-"""
+
+RAP PART:
+https://www.rappad.co/songs-about/
+https://www.rhymebuster.com/rapgenerator
+http://rapscript.net/
+https://melobytes.com/app/melobytes
+http://deepbeat.org/
+https://genius.com/discussions/155749-Rap-generator
+http://writerbot.com/lyrics
+
+
+Generate lyrics:
+
+Generate beat:
+
+Generate melody from lyrics:
+https://melobytes.com/app/melobytes
+
+Generate AVS milkdrop - python audio visualizer
+
+
+
 
 def record_voice():
-    question["voice_question"] = str(input("Whats your question? "))
-    print("voice_question recorded")
-
-def speech_to_text():
-    question["question"] = question["voice_question"]
-    print("question.soru saves as text format.")
+    # question["voice_question"] = str(input("Whats your question? "))
+    # print("voice_question recorded")
 
 def analysis_question():
     question["texts"] = question["question"].split()
@@ -94,6 +274,6 @@ def research_summary():
     text_to_speach()
     read()
 
-# research_summary()
 
-print("bo")
+# research_summary()
+"""
